d90737e2d4804623c2a85c692c07aa5a67182557
MustafaSamiShirzad <mustafasami209@gmail.com>
2023-06-26 14:36:08 +0500

    Changes made in js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,26 +12,26 @@ mobileMenuClosingBtn.addEventListener("click",()=>{
 
 let obj = {
     person1:{
-        img:"/assets/imgs/person1.jpeg",
+        img:"./assets/imgs/person1.jpeg",
         Name: "Emily Johnson",
         JobTitle: 'Software Engineer',
         Description: 'Emily is a talented software engineer with a passion for creating innovative solutions. With her strong coding skills and analytical mindset'
     },
     person2:{
-        img:"/assets/imgs/person2.jpeg",
+        img:"./assets/imgs/person2.jpeg",
         Name: 'David Thompson',
         JobTitle: 'Marketing Manager',
         Description: 'David is a strategic marketing professional known for his creative thinking and ability to drive successful campaigns.'
 
     },
     person3:{
-        img:"/assets/imgs/person3.jpeg",
+        img:"./assets/imgs/person3.jpeg",
         Name: 'Samantha Lee',
         JobTitle: 'Graphic Designer',
         Description: 'Samantha is a highly skilled graphic designer who brings ideas to life through captivating visuals. With an eye for aesthetics and attention to detail'
     },
     person4:{
-        img:"/assets/imgs/person4.jpeg",
+        img:"./assets/imgs/person4.jpeg",
         Name: 'Michael Ramirez',
         JobTitle: 'Financial Analyst',
         Description: 'Michael is a meticulous financial analyst who excels at analyzing complex data and providing valuable insights'
@@ -39,14 +39,14 @@ let obj = {
 
     },
     person5:{
-        img:"/assets/imgs/person5.jpeg",
+        img:"./assets/imgs/person5.jpeg",
         Name: 'Olivia Patel',
         JobTitle: 'Human Resources Manager',
         Description: 'Olivia is a dedicated HR manager known for her exceptional interpersonal skills and talent for fostering a positive work environment.'
 
     },
     person6:{
-        img:"/assets/imgs/person6.jpeg",
+        img:"./assets/imgs/person6.jpeg",
         Name: 'Benjamin Thompson',
         JobTitle: 'Sales Representative',
         Description: 'Benjamin is a charismatic sales representative who consistently exceeds targets and builds strong relationships with clients'
